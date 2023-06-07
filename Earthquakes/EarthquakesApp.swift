6bfdc7f1fbe65c3d6df024addb899629fd2f21c2
Kelvin Ricafort <kelvs.r@outlook.com>
2023-06-07 16:18:41 +0800

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Kelvin on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
